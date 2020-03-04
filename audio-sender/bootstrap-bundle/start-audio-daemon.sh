aed1d552ad5479846f204fd57d2f2e8159696ac4
jacek <jacek@resolvity.com>
2020-03-03 18:30:21 -0600

    updated AudioDaemon bootstrap to version 1.3.3_beta

--- audio-sender/bootstrap-bundle/start-audio-daemon.sh
+++ audio-sender/bootstrap-bundle/start-audio-daemon.sh
@@ -3,7 +3,10 @@ echo "Starting Voicegain Audio Daemon"
 
 DATE=$(date +%F_%H-%M-%S)
 APPLOG=audio-daemon.$DATE.log
+
+# Source value may have to be modified to match the correct Audio Input Device
 SOURCE=2
+
 JSON=audio-daemon-config.json
 
 echo "Starting Daemon in background with log redirected to "+$APPLOG

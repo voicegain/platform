b5f3e28ef8ce6a0cd3fedbd286d5395a64a9fb02
jacek <jacek@resolvity.com>
2020-03-20 16:32:57 -0500

    added list-audio-devices.sh

---
+++ audio-sender/bootstrap-bundle/list-audio-devices.sh
@@ -0,0 +1,1 @@
+#!/bin/bashecho "Voicegain Audio Daemon: List Audio Devices"JSON=audio-daemon-config.jsonecho "Starting Daemon in background with log redirected to "+$APPLOG./java-launch-audio-daemon.sh --json $JSON -i --log info --avc on

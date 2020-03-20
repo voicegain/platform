0986d61d98970965f6a4595e6b49ac7a813ce7c4
jacek <jacek@resolvity.com>
2020-03-20 16:35:06 -0500

    lixed eol

--- audio-sender/bootstrap-bundle/list-audio-devices.sh
+++ audio-sender/bootstrap-bundle/list-audio-devices.sh
@@ -1,1 +1,5 @@
-#!/bin/bashecho "Voicegain Audio Daemon: List Audio Devices"JSON=audio-daemon-config.jsonecho "Starting Daemon in background with log redirected to "+$APPLOG./java-launch-audio-daemon.sh --json $JSON -i --log info --avc on
+#!/bin/bash
+echo "Voicegain Audio Daemon: List Audio Devices"
+JSON=audio-daemon-config.json
+echo "Starting Daemon in background with log redirected to "+$APPLOG
+./java-launch-audio-daemon.sh --json $JSON -i --log info --avc on

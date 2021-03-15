6f2e3a48bda33a0794a9c3308636a269132d7dad
DrJJ <52052458+DrJJ@users.noreply.github.com>
2021-03-15 11:53:53 -0500

    final version of the 2-channel Twilio <Stream> example

--- examples/twilio-media-streams/twilio-click2call-transcribe-in-out-chn.py
+++ examples/twilio-media-streams/twilio-click2call-transcribe-in-out-chn.py
@@ -9,17 +9,17 @@ from twilio.rest import Client
 # Twilio credentials
 # Your Account Sid and Auth Token from twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-twilio_account_sid = 'AC054c13f4xxxxb04f2f710160'
-twilio_auth_token = '51007bf909xxxxx7e178ee973557f46'
+twilio_account_sid = 'AC054c13f4d.............74b04f2f710160'
+twilio_auth_token = 'c36eca6a180c............11498d900f6ec'
 
 # VG credentials
 # voicegain
-#JWT = "eyJhbGciOiJIUzI1xxxxR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2FwaS52b2ljZWdhaW4uYWkvdjExxxxxiJhZWRiNjAwMC0xMzdiLTQ4YzMtOWFjYi0wNDVmM2UxNjNhZmQifQ.JWTzFwk4Lorgu_Lxxxxxxwx0AnhiBwEttD2VHI"
-#vg_api_url="https://api.voicegain.ai/v1/asr/transcribe/async"
+JWT = "eyJhbGciOiJIUz....................IkpXVCJ9.eyJhdWQiOiJodHRwczovL2FwaS52b2ljZWdhaW4u...................LTQ4YzMtOWFjYi0wNDVmM2UxNjNhZmQifQ.JWTzFwk4Lor..............ttD2VHI"
+vg_api_url="https://api.voicegain.ai/v1/asr/transcribe/async"
 
 # ascalon
-JWT = "eyJhbGciOiJIUzxxxxR5cCI6IkpXVCJ9.eyJhdWQiOiIqLmFzY2FsbG9uLmFpIiwic3ViIjoiMWZiNTAzMDxxxxS00ZGMyLWFjNDctMDRkNDI3YzU1MzBmIn0.Q66m2z7hDQyGN3ivV2xxxxxnkEXf-d6AhwPgtsI2km0"
-vg_api_url="https://api.ascalon.ai/v1/asr/transcribe/async"
+#JWT = "eyJhbGciOiJIUz.............R5cCI6IkpXVCJ9.eyJhdWQiOiIqLmFzY2FsbG9uLmF..........................jNDctMDRkNDI3YzU1MzBmIn0.pu3Cv5COVghH4....................7LE-n_uB6tTao"
+#vg_api_url="https://api.ascalon.ai/v1/asr/transcribe/async"
 
 
 headers = {"Authorization":JWT}
@@ -74,21 +74,31 @@ body = {
 }
 
 print("Connecting to: "+vg_api_url, flush=True)
-init_response_raw = requests.post(vg_api_url, json=body, headers=headers)
+vg_response = requests.post(vg_api_url, json=body, headers=headers)
+print("response code: "+str(vg_response.status_code))
+if(200 != vg_response.status_code):
+  print(vg_response.content)
+  sys.exit("Received error response from http")
+
 try:
-    init_response = init_response_raw.json()
+    vg_response_body = vg_response.json()
 except:
-    print(str(init_response_raw))
-    sys.exit("Received error response from http")
+    sys.exit("Response from http has no JSON body")
+
 
 # retrieve values from response
 # sessionId and capturedAudio are printed for debugging purposes
-session_id_left = init_response["sessions"][0]["sessionId"]
-session_id_right = init_response["sessions"][1]["sessionId"]
-ws_url_left = init_response["sessions"][0]["websocket"]["url"]
-ws_url_right = init_response["sessions"][1]["websocket"]["url"]
-audio_ws_url = init_response["audio"]["stream"]["websocketUrl"]
-capturedAudio = init_response["audio"].get("capturedAudio")
+try:
+  session_id_left = vg_response_body["sessions"][0]["sessionId"]
+  session_id_right = vg_response_body["sessions"][1]["sessionId"]
+  ws_url_left = vg_response_body["sessions"][0]["websocket"]["url"]
+  ws_url_right = vg_response_body["sessions"][1]["websocket"]["url"]
+  audio_ws_url = vg_response_body["audio"]["stream"]["websocketUrl"]
+  capturedAudio = vg_response_body["audio"].get("capturedAudio")
+except Exception as e:
+  print("Error getting parameters from response: "+str(e))
+  print(vg_response_body)
+  sys.exit("Bad body !?")
 
 print("           SessionId L: {}".format(session_id_left))
 print("           SessionId R: {}".format(session_id_right))
@@ -98,6 +108,8 @@ if( not(capturedAudio is None)):
 print("Result Websocket Url L: {}".format(ws_url_left), flush=True)
 print("Result Websocket Url R: {}".format(ws_url_right), flush=True)
 
+# TwiML command to pass to Twilio client
+# the key thing is the <Start> <Stream> which will send inbound and outbound audio to Voicegain 
 twiml_cmd = ' \
 <Response> \n \
     <Say voice="woman" language="en-US">connected </Say> \n \
@@ -113,18 +125,11 @@ twiml_cmd = ' \
     <Pause length="1"/> \n \
     <Say voice="woman" language="en-US">outbound, channel, speaking.</Say> \n \
     <Pause length="1"/> \n \
-    <Say voice="woman" language="en-US">outbound, channel, speaking.</Say> \n \
-    <Pause length="1"/> \n \
-    <Say voice="woman" language="en-US">outbound, channel, speaking.</Say> \n \
-    <Pause length="1"/> \n \
-    <Say voice="woman" language="en-US">outbound, channel, speaking.</Say> \n \
-    <Pause length="1"/> \n \
-    <Say voice="woman" language="en-US">outbound, channel, speaking.</Say> \n \
-    <Pause length="1"/> \n \
     <Say voice="woman" language="en-US">the end </Say> \n \
     <Pause length="1"/> \n \
 </Response>'
 
+# keep tranck of number of websocket messages received from Voicegain
 msgCnt = 0
 
 # function to process JSON with incremental transcription results sent as messages over websocket
@@ -158,7 +163,7 @@ def process_ws_msg(wsMsg, stack, prefix):
   except Exception as e: 
     print("ERROR: "+str(e), flush=True)
 
- # thread that connects to the websocket and receives the results
+# thread that connects to the websocket and receives the results
 # we do it in a separate thread because in the main thread we are streaming the audio
 class wsThread (threading.Thread):
    def __init__(self, ws_uri, prefix):
@@ -186,9 +191,11 @@ async def websocket_receive(uri, stack, prefix):
           print(e, flush=True)  
 
 
-# create and start the websocket thread
-threadWsLeft = wsThread(ws_url_left, "L")
-threadWsRight = wsThread(ws_url_right, "R")
+# create and start the websocket threads to receive results from Voicegain
+# inbound telephone channel
+threadWsLeft = wsThread(ws_url_left, "L >>\t")
+# outbound telephone channel
+threadWsRight = wsThread(ws_url_right, "R <<")
 threadWsLeft.start()
 threadWsRight.start()
 
@@ -199,15 +206,16 @@ client = Client(twilio_account_sid, twilio_auth_token)
 print("Calling Twilio")
 print(twiml_cmd, flush=True)
 
-
+# set the phone numbers
 call = client.calls.create(
                         twiml=twiml_cmd,
-                        to='+18174002096',
-                        from_='+14694895554'
+                        to='+1817.....96',
+                        from_='+1469.....54'
                     )
 
 print("Twilio call sid: "+call.sid, flush=True)
 
+# show the status of the Twilio call until it is complete
 while True:
     call = client.calls(call.sid).fetch()
     print(call.status+" msgs rcvd "+str(msgCnt), flush=True)

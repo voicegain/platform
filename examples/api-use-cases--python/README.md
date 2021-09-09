eb90a69565cedec785aa0fdbb11b44eeb0850125
DrJJ <52052458+DrJJ@users.noreply.github.com>
2021-09-09 15:41:31 -0500

    added async-off-line-via-file-upload-bulk.py

--- examples/api-use-cases--python/README.md
+++ examples/api-use-cases--python/README.md
@@ -3,10 +3,12 @@
 These 4 examples are similar in that they all use **async** api and also use **polling** to obtain the result of transcription. Here are the differences: 
 * async-off-line-from-url.py - OFF-LINE trancription of audio file retrieved from URL
 * async-off-line-via-file-upload.py - OFF-LINE trancription of audio file uploaded using /data API (multipart/form-data)
+retrieved from URL - this example processes a single file and shows details of all polling request
+* async-off-line-via-file-upload-bulk.py - this is similar to the example above but processes all files from a directory and stores only the final results in text format (no intermediate polling results are stored). This is a handy script to process a lot of files in offline mode. Note: it submits 1 file at a time so it is not optimized for throughput.  
 * async-real-time-word-tree.py - REAL-TIME transcription of audio file retrieved from URL. It uses the word-tree output which provides more recognition alternatives/hypotheses.
-* async-real-time-from-websocket-with-polling.py - REAL-TIME transcription of audio file streamed via a websocket. The transcript results (incremental and full) are retrieved via polling. This script is setup to process all audio input files from input directory and report results in a single text file.
+* async-real-time-from-websocket-with-polling.py - REAL-TIME transcription of audio file streamed via a websocket. The transcript results (incremental and full) are retrieved via polling. This script is setup to process all audio input files from input directory and report results in a single text file. It transcodes input audio files to a format suitable for websocket streaming, e.g. mulaw. 
 
 # Install
 To run these scripts you will need Python (we have tested the scripts using python 3.8). You will need to install the required dependencies using `pip`.
 
-The last script requires ffmpeg. This [video](https://www.youtube.com/watch?v=HmoCT7Km0wo) may be useful for Windows 10 users in demonstrating how to setup Git Bash, Python, and FFMpeg.  
+The last script requires **ffmpeg**. This [video](https://www.youtube.com/watch?v=HmoCT7Km0wo) may be useful for Windows 10 users in demonstrating how to setup Git Bash, Python, and FFMpeg.  
---
+++ examples/api-use-cases--python/async-off-line-via-data-upload-bulk.py
@@ -0,0 +1,152 @@
+import requests, time, os, json, re
+
+
+## specify here the directory with input audio files to test
+input_path = "./my-files/"
+
+spanish = True
+
+#voicegain
+platform = "voicegain"
+JWT = "< JWT token obtained from Web Console https://console.voicegain.ai >"
+
+asr_body = {
+    "sessions": [
+        {
+            "asyncMode": "OFF-LINE",
+            "poll": {
+                "afterlife": 60000,
+                "persist": 120000
+            },
+            "content": {
+                "incremental": ["progress"],
+                "full" : ["words"]
+            }
+        }
+    ],
+    "audio":{
+        "source": {
+            "dataStore": {
+                "uuid": "to be filled later"
+            }
+        }
+    },
+    "settings" : {
+        "formatters" : [
+            {"type" : "digits"}
+        ],
+        "asr" : {
+            "sensitivity" : 0.3,
+            "speedVsAccuracy" : 0.5
+        }
+    }
+}
+
+#### all settings above this line ####
+
+if(spanish):
+    asr_body["settings"]["asr"]["acousticModelNonRealTime"] = "VoiceGain-ol-es"
+
+audio_type = "audio/wav"
+
+output_path = "output-{}".format(time.strftime("%Y-%m-%d_%H-%M-%S"))
+
+data_url = "https://api.{}.ai/v1/data/file?reuse=true&transcode=enable".format(platform)
+
+headers = {"Authorization":JWT}
+
+def process_one_file(audio_fname):
+
+    path, fname = os.path.split(audio_fname)
+
+    print("Processing {}/{}".format(path,fname), flush=True)
+
+    data_body = {
+        "name" : re.sub("[^A-Za-z0-9]+", "-", fname),
+        "description" : audio_fname,
+        "contentType" : audio_type,
+        "tags" : ["test"]
+    }
+
+    multipart_form_data = {
+        'file': (audio_fname, open(audio_fname, 'rb'), audio_type),
+        'objectdata': (None, json.dumps(data_body), "application/json")
+    }
+    print("uploading audio data {} ...".format(audio_fname), flush=True)
+    data_response = requests.post(data_url, files=multipart_form_data, headers=headers).json()
+    print("data response: {}".format(data_response), flush=True)
+    object_id = data_response["objectId"]
+    print("objectId: {}".format(object_id), flush=True)
+
+    ## set the audio id in the asr request
+    asr_body["audio"]["source"]["dataStore"]["uuid"] = object_id
+
+
+    print("making asr request ...", flush=True)
+    asr_response = requests.post("https://api.{}.ai/v1/asr/transcribe/async".format(platform), json=asr_body, headers=headers).json()
+    session_id = asr_response["sessions"][0]["sessionId"]
+    polling_url = asr_response["sessions"][0]["poll"]["url"]
+
+    print("sessionId: {}".format(session_id), flush=True)
+    print(" poll.url: {}".format(polling_url), flush=True)
+
+    
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+
+    index = 0
+    while True:
+        if(index<5):
+            time.sleep(0.3)
+        else:
+            time.sleep(4.9)
+        poll_response = requests.get(polling_url+"?full=false", headers=headers).json()
+        phase = poll_response["progress"]["phase"]
+        is_final = poll_response["result"]["final"]
+        print("Phase: {} Final: {}".format(phase, is_final), flush=True)
+        # write poll_response to JSON
+        # poll_response_path = os.path.join(output_path, "{}-{}-{}.json".format(audio_fname, session_id, index))
+        # with open(poll_response_path, 'w') as outfile:
+        #     json.dump(poll_response, outfile)
+        # print("Phase: {} Final: {} -> Save result to {}".format(phase, is_final, poll_response_path), flush=True)
+
+        index += 1
+        if is_final:
+            break
+
+    # poll_response = requests.get(polling_url+"?full=true", headers=headers).json()
+    # # write poll_response to JSON
+    # poll_response_path = os.path.join(output_path, "{}--{}.json".format(session_id, index))
+    # with open(poll_response_path, 'w') as outfile:
+    #     json.dump(poll_response, outfile)
+    # print("Save final result to {}".format(poll_response_path), flush=True)
+
+    # get result as text file
+
+    txt_url = "https://api.{}.ai/v1/asr/transcribe/{}/transcript?format=text".format(platform, session_id)
+    print("Retrieving transcript using url: {}".format(txt_url), flush=True)
+    txt_response = requests.get(txt_url, headers=headers)
+    transcript_text_path = os.path.join(output_path, "{}.txt".format(fname))
+    with open(transcript_text_path, 'w') as file_object:
+        file_object.write(txt_response.text)
+    print("Save final transcript text to {}".format(transcript_text_path), flush=True)
+
+
+## MAIN ##
+
+print("START", flush=True)
+
+list_of_files = []
+
+for root, dirs, files in os.walk(input_path):
+	for file in files:
+		list_of_files.append(os.path.join(root,file))
+
+print("files to test")
+for name in list_of_files:
+    print(name)
+
+for name in list_of_files:
+    process_one_file(name)
+
+print("THE END", flush=True)

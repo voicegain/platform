e2ffc3aa76a87de9d26f1ed3e381a6d90132b782
DrJJ <52052458+DrJJ@users.noreply.github.com>
2021-10-29 14:15:19 -0500

    simple python lambda function for an echo bot

---
+++ examples/voicebot-lambda-twilio-vg-rasa/lambda-echo-bot.py
@@ -0,0 +1,26 @@
+import json
+
+print('Loading function')
+
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    response = []
+    try:
+        rawRequest = event['body']
+        
+        print("Raw request: "+rawRequest)
+        
+        request = json.loads(rawRequest);
+        
+        msg = request.get('message')
+        msgStr = str(msg)
+        
+        response.append({"text" : "You said:"})
+        response.append({"text" : msgStr})
+
+    except Exception as e:
+        eStr = str(e)
+        response.append({"text" : eStr})
+        
+    return response

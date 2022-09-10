6d0a38117aa65230be070cacbfdbd1d6e83345a6
DrJJ <52052458+DrJJ@users.noreply.github.com>
2022-09-09 22:35:11 -0500

    improved handling of bad requests

--- examples/twilio-recordings/AWS-lambda-for-encrypted-recordings.py
+++ examples/twilio-recordings/AWS-lambda-for-encrypted-recordings.py
@@ -37,8 +37,7 @@ headers = {"Authorization":voicegainJwt, "Content-Type" : "application/json"}
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
     if(event.get("resource") is not None):
-        if(event["resource"] == "/TwilioRecordingCallback"):
-            print("Got: TwilioRecordingCallback")
+        if(event.get('body') is not None):
             try:    
                 rawRequest = event['body']
                 
@@ -52,117 +51,129 @@ def lambda_handler(event, context):
                 
                 print(json.dumps(twilioParams))
                 
-                twilioRecordingStatus = twilioParams["RecordingStatus"][0];
-                print("Recording Status: "+twilioRecordingStatus)
-                
-                if(twilioRecordingStatus == "completed"):                
-                
-                    encryptionDetailsJson = twilioParams.get("EncryptionDetails")
+                if(twilioParams.get("RecordingStatus") is not None):
+                    twilioRecordingStatus = twilioParams["RecordingStatus"][0];
+                    print("Recording Status: "+twilioRecordingStatus)
                     
-                    if(encryptionDetailsJson is not None):
-                        print("Recording is encrypted")
+                    if(twilioRecordingStatus == "completed"):                
                     
-                        encryptionDetails = json.loads(encryptionDetailsJson[0])
+                        encryptionDetailsJson = twilioParams.get("EncryptionDetails")
                         
-                        print(json.dumps(encryptionDetails))
+                        if(encryptionDetailsJson is not None):
+                            print("Recording is encrypted")
                         
-                        publicKeyId = encryptionDetails.get('public_key_sid')
-                        
-                        if(expectedPublicKeyId == publicKeyId):
-                            print("Public key Id matches")
-
-
-                            twilioRecordingUrl = twilioParams["RecordingUrl"][0] + ".json"
+                            encryptionDetails = json.loads(encryptionDetailsJson[0])
                             
-                            print("Recording URL: "+twilioRecordingUrl)
+                            print(json.dumps(encryptionDetails))
                             
-                            twilioRecordingChannels = twilioParams["RecordingChannels"][0];
-                            print("Recording Channels: "+twilioRecordingChannels)
+                            publicKeyId = encryptionDetails.get('public_key_sid')
                             
-                            formatChannels = "mono"
-                            audioChannelSelector = "mix"
-                            if(twilioRecordingChannels=="2"):
-                                formatChannels = "stereo"
-                                audioChannelSelector = "two-channel"
-                            
-                            twilioRecordingTrack = twilioParams["RecordingTrack"][0];
-                            twilioRecordingSource = twilioParams["RecordingSource"][0]
-                            twilioRecordingStartTime = twilioParams["RecordingStartTime"][0];
-                            twilioRecordingDuration = twilioParams["RecordingDuration"][0];
-                            twilioAccountSid = twilioParams["AccountSid"][0];
-                            twilioCallSid = twilioParams["CallSid"][0];
-                            twilioRecordingSid = twilioParams["RecordingSid"][0];
-                            
-                            
-                            vgRequest = {
-                                "sessions" : [{
-                                    "asyncMode" : "OFF-LINE",
-                                    "audioChannelSelector" : audioChannelSelector,
-                                    "vadMode" :"normal",
-                                    
-                                    # this will send it to the portal which is fine for testing
-                                    # for prod you would likely configure a callback
-                                    "portal" : {"label" : "Foo", "persist" : 3600000},
-                                    
-                                    "diarization" : {"minSpeakers" : 1, "maxSpeakers" : 1},
-                                    "metadata" : [
-                                        {"name" : "twilioAccountSid",           "value" : twilioAccountSid},
-                                        {"name" : "twilioCallSid",              "value" : twilioCallSid},
-                                        {"name" : "twilioRecordingSid",         "value" : twilioRecordingSid},
-                                        {"name" : "twilioRecordingStartTime",   "value" : twilioRecordingStartTime},
-                                        {"name" : "twilioRecordingDuration",    "value" : twilioRecordingDuration},
-                                        {"name" : "twilioRecordingSource",      "value" : twilioRecordingSource},
-                                        {"name" : "twilioRecordingTrack",       "value" : twilioRecordingTrack},
-                                        {"name" : "twilioRecordingChannels",    "value" : twilioRecordingChannels},
-                                        {"name" : "twilioRecordingUrl",         "value" : twilioRecordingUrl}
-                                    ]
-                                }],
-                                "audio" :{
-                                    "source" :{
-                                        "fromUrl" : {
-                                            "url" : twilioRecordingUrl,
-                                            "authConf" : myAuthConf
-                                        }
+                            if(expectedPublicKeyId == publicKeyId):
+                                print("Public key Id matches")
+    
+                                twilioRecordingUrl = twilioParams["RecordingUrl"][0] + ".json"
+                                print("Recording URL: "+twilioRecordingUrl)
+                                
+                                twilioRecordingChannels = twilioParams["RecordingChannels"][0];
+                                print("Recording Channels: "+twilioRecordingChannels)
+                                
+                                formatChannels = "mono"
+                                audioChannelSelector = "mix"
+                                if(twilioRecordingChannels=="2"):
+                                    formatChannels = "stereo"
+                                    audioChannelSelector = "two-channel"
+                                
+                                twilioRecordingTrack = twilioParams["RecordingTrack"][0];
+                                twilioRecordingSource = twilioParams["RecordingSource"][0]
+                                twilioRecordingStartTime = twilioParams["RecordingStartTime"][0];
+                                twilioRecordingDuration = twilioParams["RecordingDuration"][0];
+                                twilioAccountSid = twilioParams["AccountSid"][0];
+                                twilioCallSid = twilioParams["CallSid"][0];
+                                twilioRecordingSid = twilioParams["RecordingSid"][0];
+                                
+                                vgRequest = {
+                                    "sessions" : [{
+                                        "asyncMode" : "OFF-LINE",
+                                        "audioChannelSelector" : audioChannelSelector,
+                                        "vadMode" :"normal",
+                                        
+                                        # this will send it to the portal which is fine for testing
+                                        # for prod you would likely configure a callback
+                                        "portal" : {"label" : "Foo", "persist" : 3600000},
+                                        
+                                        "diarization" : {"minSpeakers" : 1, "maxSpeakers" : 1},
+                                        "metadata" : [
+                                            {"name" : "twilioAccountSid",           "value" : twilioAccountSid},
+                                            {"name" : "twilioCallSid",              "value" : twilioCallSid},
+                                            {"name" : "twilioRecordingSid",         "value" : twilioRecordingSid},
+                                            {"name" : "twilioRecordingStartTime",   "value" : twilioRecordingStartTime},
+                                            {"name" : "twilioRecordingDuration",    "value" : twilioRecordingDuration},
+                                            {"name" : "twilioRecordingSource",      "value" : twilioRecordingSource},
+                                            {"name" : "twilioRecordingTrack",       "value" : twilioRecordingTrack},
+                                            {"name" : "twilioRecordingChannels",    "value" : twilioRecordingChannels},
+                                            {"name" : "twilioRecordingUrl",         "value" : twilioRecordingUrl}
+                                        ]
+                                    }],
+                                    "audio" :{
+                                        "source" :{
+                                            "fromUrl" : {
+                                                "url" : twilioRecordingUrl,
+                                                "authConf" : myAuthConf
+                                            }
+                                        },
+                                        "channels" : formatChannels
                                     },
-                                    "channels" : formatChannels
-                                },
-                                "settings" :{
-                                    "asr" : {
-                                        "languages" : ["en"]
-                                    },
-                                    "formatters" : [
-                                        {"type" : "digits"}
-                                    ]
+                                    "settings" :{
+                                        "asr" : {
+                                            "languages" : ["en"]
+                                        },
+                                        "formatters" : [
+                                            {"type" : "digits"}
+                                        ]
+                                    }
                                 }
-                            }
-                        
-                            http = urllib3.PoolManager()
-    
-                            print("Making request to: "+voicegainUrl )
                             
-                            asr_response_raw = http.urlopen("POST", voicegainUrl, headers=headers, body=json.dumps(vgRequest))
-    
-                            print("response code from VG: "+str(asr_response_raw.status))
-                            decoded_r = asr_response_raw.data.decode("utf8")
-                            print("response body from VG: "+str(decoded_r))
-
+                                http = urllib3.PoolManager()
+        
+                                print("Making request to: "+voicegainUrl )
+                                
+                                asr_response_raw = http.urlopen("POST", voicegainUrl, headers=headers, body=json.dumps(vgRequest))
+        
+                                print("response code from VG: "+str(asr_response_raw.status))
+                                if(asr_response_raw.status == 200):
+                                    decoded_r = asr_response_raw.data.decode("utf8")
+                                    print("response body from VG: "+str(decoded_r))
+                                    return {
+                                        'statusCode': 200,
+                                        'body': json.dumps('OK')
+                                    }        
+                                else:
+                                    return {
+                                        'statusCode': 200,
+                                        'body': json.dumps('OK but Voicegain request returned code '+str(asr_response_raw.status))
+                                    }                                       
+                            else:
+                                return {
+                                    'statusCode': 400,
+                                    'body': json.dumps('Unexpected public key Id: '+publicKeyId)
+                                }                    
                         else:
                             return {
                                 'statusCode': 400,
-                                'body': json.dumps('Unexpected public key Id: '+publicKeyId)
+                                'body': json.dumps('Not an encrypted recording')
                             }                    
                     else:
+                        print("Cannot invoke Voicegain API because recording is not complete")
                         return {
-                            'statusCode': 400,
-                            'body': json.dumps('Not an encrypted recording')
-                        }                    
+                            'statusCode': 200,
+                            'body': json.dumps('OK but not completed')
+                        }                              
                 else:
-                    print("Cannot invoke Voicegain API because recording is not complete")
-                
-                return {
-                    'statusCode': 200,
-                    'body': json.dumps('OK')
-                }
+                    return {
+                        'statusCode': 200,
+                        'body': json.dumps('No Twilio RecordingStatus')
+                    }                        
+
             except Exception as e:
                 eStr = str(e)
                 print("Exception: "+eStr)
@@ -173,9 +184,8 @@ def lambda_handler(event, context):
         else:
             return {
                 'statusCode': 400,
-                'body': json.dumps('Not /TwilioRecordingCallback')
-            }
-    
+                'body': json.dumps('No body')
+            }                    
     else:
         return {
             'statusCode': 400,

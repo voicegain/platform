0fecf1d1c6364f763f10a39b7cfece581cee1c6c
shivamparashar <shivam.parashar@voicegain.ai>
2024-02-05 21:54:05 +0530

    add echo bot script

--- telephony-bot-api/type-websocket/examples/echo-bot/script_telephony_bot_ws.py
+++ telephony-bot-api/type-websocket/examples/echo-bot/script_telephony_bot_ws.py
@@ -9,115 +9,175 @@ import json
 import requests
 import threading
 import time
+from enum import Enum
 
 
 app = Flask(__name__)
 
 
-sequence = 0
+class MessageTypes(Enum):
+    PING = "ping",
+    NEW_AIVR_SESSION = "new_aivr_session"
+    AIVR_EVENT = "aivr_event"
+    AIVR_VARS_CHANGED = "aivr_vars_changed"
+    WS_WORD = "ws_word"
+    WORD_CORRECTION = "word_correction"
+    SEGMENT_HYPOTHESIS_OR_RECOGNITION = "segment_hypothesis_or_recognition"
+    NOT_FOUND = "not_found"
 
-resp1 = {
-    'sequence': -1,
-    'prompt': {
-        'text': 'welcome to voicegain echo bot'
-    }
-}
 
-async def main(uri):
-    print("Inside main, connecting to uri: ", uri)
-    async with websockets.connect(uri, write_limit=128,
-        compression=None) as websocket:
-        print("Connected to uri: ", uri)
-        while(True):
-            print("Waiting to receive msg")
-            try:
-                global sequence
-                received_msg = await websocket.recv()
-                print("received msg ", received_msg)
-                # we assume all the received messages are JSON
-                received = json.loads(received_msg)
-                if 'ping' in received:
-                    print("ping received - ignore")
-                elif 'sid' in received:
-                    print("first message received")
-                    sequence = received['sequence']
-                    print("sequence is ", sequence)
-                    if(sequence == 1):
-                        resp1["sequence"] = sequence
-                        await websocket.send(json.dumps(resp1))
-                        print("first prompt is sent")
-                    else:
-                        print("sequence is not 1 - weird")
-                else:
-                    ## here we should do the echo question
-                    sequence += 1
-                    resp1["sequence"] = sequence
-                    await websocket.send(json.dumps(resp1))
-                    print("Message is sent")
-            except Exception as e:
-                print("ex in main")
-                print(e)
-                break
 
+
+def get_received_msg_type(received_msg):
+
+   if(received_msg.get("ping") is not None):
+      return MessageTypes.PING
+
+   elif(received_msg.get("sid") is not None):
+      return MessageTypes.NEW_AIVR_SESSION
+
+   elif(received_msg.get("event") is not None):
+      if(received_msg.get("event").get("type") == "input"):
+         return MessageTypes.AIVR_EVENT
+   
+   elif(received_msg.get("vars") is not None):
+      return MessageTypes.AIVR_VARS_CHANGED
+
+   elif(received_msg.get("utt") is not None):
+      return MessageTypes.WS_WORD
+   
+   elif(received_msg.get("del") is not None):
+      return MessageTypes.WORD_CORRECTION
+
+   elif(received_msg.get("type") is not None):
+      return MessageTypes.SEGMENT_HYPOTHESIS_OR_RECOGNITION
+
+   return MessageTypes.NOT_FOUND
+
+
+
+
+# Responses:
+new_aivr_session_response = {
+      "question": {
+      "text": "This is an echo bot. Say Something..."
+      }
+   }
+
+
+aivr_response = {
+      "prompt": {
+      "text": ""
+      }
+   }
+
+
+aivr_disconnect_response = {
+
+      "disconnect": {
+      "reason": "Successfully received feedback",
+      "prompt": {
+         "text": "Good Bye!!"
+         }
+      }
+   }
+
+
+
+async def respond_to_new_aivr_session(websocket):
+   await websocket.send(json.dumps(new_aivr_session_response))
+
+
+
+async def respond_to_aivr_event(websocket, received_msg):
+   #Echo the same message back
+   message = received_msg["event"]["vuiAlternatives"][0]["utterance"]
+   aivr_response["prompt"]["text"] = message
+   await websocket.send(json.dumps(aivr_response))
+
+
+
+async def respond_to_aivr_disconnect_event(websocket):
+   await websocket.send(json.dumps(aivr_disconnect_response))
+
+
+
+async def run_echo_bot(uri):
+    async with websockets.connect(uri, write_limit=128, compression=None) as websocket:
+       while(True):
+         print("Receiving message...")
+         received_msg_str = await websocket.recv()
+         received_msg = json.loads(received_msg_str)
+         print("Received message: ", received_msg)
+         
+         msg_type = get_received_msg_type(received_msg)
+         
+         if(msg_type == MessageTypes.PING):
+            continue
+
+         if(msg_type == MessageTypes.NEW_AIVR_SESSION):
+            await respond_to_new_aivr_session(websocket)
+
+         if(msg_type == MessageTypes.AIVR_EVENT):
+            print("Sending the same message back...")
+            await respond_to_aivr_event(websocket, received_msg)
+            print("Disconnecting... Goodbye!!")
+            await respond_to_aivr_disconnect_event(websocket)
 
 
 # Function websocket_flow_main, recv and sends the results over websocket
 def websocket_flow_main(uri):
-    print(f"Inside websocket_flow_main, uri={uri}")
-    try:
-        asyncio.run(main(uri))
-        print("Finish websocket_flow_main . ")
-    except Exception as e:
-        print("ex in websocket_flow_main")
-        print(e)
+   try:
+      asyncio.run(run_echo_bot(uri))
+   except Exception as e:
+      print("Exception caught within websocket_flow_main", e)
 
 
 
-@app.route('/', methods=["POST"])
+@app.route("/", methods=["POST"])
 def post():
-    print("Post req received. ")
-    payload = request.json
-    print("POST Req!! Payload is: " + str(payload))
-    wsUrl = payload["wsUrl"]
-    print("POST Req!! wsUrl is: " + str(wsUrl))
-    # Create a WebSocket connection
-    try:
-        # In the first post request, we fetched the wsUrl and began receiving and sending messages.
-        # Start a thread to call websocket_flow_send_main
-        thread = threading.Thread(target=websocket_flow_main, args=(wsUrl,))
-        thread.start()
-    except Exception as e: 
-        print(e)
-    data = {"response": "accept"}
-    print(f"POST Req returning {data}")
-    return jsonify(data)
+   global ws_url_final
+   print("Post req recieved. ")
+   payload = request.json
+   print("POST Req!! Payload is: " + str(payload))
+   # Create a WebSocket connection
+   try:
+      # In the first post request, we fetched the wsUrl and began receiving and sending messages.
+      ws_url_final = payload["wsUrl"]
+      # Start a thread to call websocket_flow_send_main
+      thread = threading.Thread(target=websocket_flow_main, args=(payload["wsUrl"],))
+      thread.start()
+   except Exception as e: 
+      print(e)
+   data = {"response": "accept"}
+   print("POST Req returning")
+   return jsonify(data)
 
 
 
-@app.route('/', methods=["DELETE"])
+@app.route("/", methods=["DELETE"])
 def delete():
-    print("Del req recieved.")
-    seq = request.args.get('seq', type=int)
-    print("Del Req!! seq is: " + str(seq))
-    # Create a WebSocket connection
-    print("Exiting DELETE Req"+str(datetime.datetime.now()))
-    data = {"termination": "normal"}
-    return jsonify(data)
+   print("Del req recieved.")
+   seq = request.args.get("seq", type=int)
+   print("Del Req!! seq is: " + str(seq))
+   # Create a WebSocket connection
+   print("Exiting DELETE Req"+str(datetime.datetime.now()))
+   data = {"termination": "normal"}
+   return jsonify(data)
 
 
 
-@app.route('/', methods=["GET"])
+@app.route("/", methods=["GET"])
 def get():
-    return "GET req received."
+   return "GET req recieved."
 
 
-
-@app.route('/run')
+@app.route("/run")
 def run():
-    return "Run, VG.AI!"
+   return "Run, Shivam VG.AI!"
 
-if __name__ == '__main__':
-    app.run(port=8084)
+if __name__ == "__main__":
+   app.run(port=80)
 
 
 

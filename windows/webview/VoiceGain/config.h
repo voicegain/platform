02f4c61eb65768337700237271630d1f92f86901
Neel Kolhe <41355538+codemstrneel@users.noreply.github.com>
2024-07-12 16:52:28 -0700

    Create config.h

---
+++ windows/webview/VoiceGain/config.h
@@ -0,0 +1,11 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const int WINDOW_WIDTH = 1000;
+const int WINDOW_HEIGHT = 600;
+const wchar_t WINDOW_TITLE[] = L"VoiceGain Demo";
+const double ZOOM_LEVEL = 1.0; // Add a constant for the zoom level
+const wchar_t NAVIGATE_URL[] = L"https://demo.voicegain.ai/voicebot"; // Add a constant for the URL
+
+#endif // CONFIG_H

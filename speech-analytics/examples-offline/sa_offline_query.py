aa9252aa6996c65ef20bdce9e2caf2338088e8ef
Filip Jarmulak <fili@voicegain.ai>
2025-05-14 10:55:57 +0200

    Changed some print functions around for clearer info

--- speech-analytics/examples-offline/sa_offline_query.py
+++ speech-analytics/examples-offline/sa_offline_query.py
@@ -52,7 +52,19 @@ sa_session_body = {
 }
 sa_session_id = None
 
-#
+# Modify this to get the data you want. True means the data is returned, False means it is not.
+sa_data_params = {
+    'words': True,
+    'audio': True,
+    'meta': True,
+    'wordcloud': True,
+    'summary': True,
+    'keywords': True,
+    'entities': True,
+    'phrases': True,
+}
+
+
 sa_query_params = {
     'fromAllContexts': False,
     'limit': 10,
@@ -87,7 +99,7 @@ def delete_sa_session(id):
 
 
 # Test function
-def test(audio, sa_session, sa_query):
+def test(audio, sa_session, sa_query, sa_data):
     # 1. Upload audio to datastore
     upload_audio = requests.post(
         url + '/data/file',
@@ -97,14 +109,16 @@ def test(audio, sa_session, sa_query):
 
     print('File Upload...')
     print(f'Status code: {upload_audio.status_code}')
-    print(f'Info: {upload_audio.json()}')
 
     if upload_audio.status_code != 200:
         exit()
         
+    print(f'Audio ID: {audio_id}')
     audio_id = upload_audio.json()['objectId']
 
     # 2. Create SA session
+    sa_session["audio"][0]["source"]["dataObjectUuid"] = audio_id
+
     create_sa_session = requests.post(
         url + '/sa/offline',
         headers={'Authorization': jwt},
@@ -119,9 +133,62 @@ def test(audio, sa_session, sa_query):
         delete_audio(audio_id)
         exit()
 
+    print(f'SA Session ID: {create_sa_session.json()["saSessionId"]}')
     sa_session_id = create_sa_session.json()['saSessionId']
 
-    # 3. Query SA sessions
+    # 3. Poll for SA session status until done or error
+    print('Polling for SA session status...')
+    polls = 0
+    while True:
+        polls += 1
+        get_sa_session = requests.get(
+            url + '/sa/offline/' + sa_session_id,
+            headers={'Authorization': jwt},
+        )
+
+        if get_sa_session.status_code == 200:
+            if get_sa_session.json()['progress']['phase'] == 'DONE':
+                print('SA Session Done!')
+                print(f'Status code: {get_sa_session.status_code}')
+                #print(f'Info: {get_sa_session.json()}')
+                break
+            elif get_sa_session.json()['progress']['phase'] == 'ERROR':
+                print('Error while processing SA session:')
+                print(f'Status code: {get_sa_session.status_code}')
+                print(f'Info: {get_sa_session.json()}')
+                break
+            else:
+                print('SA Session still processing...')
+                print(f'Status code: {get_sa_session.status_code}')
+                #print(f'Info: {get_sa_session.json()}')
+                print('Sleeping for 10 seconds...')
+        else:
+            print('Error getting SA session:')
+            print(f'Status code: {get_sa_session.status_code}')
+            print(f'Info: {get_sa_session.json()}')
+            break
+
+        if polls >= max_polls:
+            print('Max number of polls reached. Deleting SA session...')
+            delete_sa_session(sa_session_id)
+            delete_audio(audio_id)
+            exit()
+        
+        time.sleep(sleep_time)
+
+    # 4. Get SA session Data:
+    if get_sa_session.status_code == 200:
+        get_sa_session_data = requests.get(
+            url + '/sa/offline/' + sa_session_id + '/data',
+            headers={'Authorization': jwt},
+            params=sa_data
+        )
+
+        print('Getting SA session data...')
+        print(f'Status code: {get_sa_session_data.status_code}')
+        print(f'Info: {get_sa_session_data.json()}')
+
+    # 5. Query SA sessions
     query_sa_sessions = requests.get(
         url + '/sa/offline',
         headers={'Authorization': jwt},
@@ -132,9 +199,9 @@ def test(audio, sa_session, sa_query):
     print(f'Status code: {query_sa_sessions.status_code}')
     print(f'Info: {query_sa_sessions.json()}')
 
-    # 4. Final cleanup
+    # 6. Final cleanup
     delete_sa_session(sa_session_id)
     delete_audio(audio_id)
     print('Test complete!')
 
-test(audio_body, sa_session_body, sa_query_params)
+test(audio_body, sa_session_body, sa_query_params, sa_data_params)
